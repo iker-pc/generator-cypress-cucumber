ad905c9838548ddf0999d5daace094d0bdcdf26b
Iker Perez <iker.perez.casajus@gmail.com>
2025-12-02 09:32:33 +0100

    fix project instalation using npm

--- src/const/questions/name.ts
+++ src/const/questions/name.ts
@@ -4,7 +4,6 @@ const nameQuestion: CustomPrompQuestion = {
     type: "input",
     name: "name",
     message: "What's is the name of your project?",
-    required: true,
 }
 
 export { nameQuestion };
